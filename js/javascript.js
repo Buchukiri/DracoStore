63acf654455dbf4d49fb529f2292b6afd55c1b2c
Franck Lebourgeois <franck.lebourgeois@gmail.com>
2022-06-29 13:21:01 +0200

    create object for articles

---
+++ js/javascript.js
@@ -0,0 +1,41 @@
+const articles = {
+    potion : {
+        prix : 2,
+        quantité : 10
+    },
+    elixir : {
+        prix : 3,
+        quantité : 10
+    },
+    dague : {
+        prix : 12,
+        quantité : 10
+    },
+    fleche : {
+        prix : 1,
+        quantité : 10
+    },
+    pantalon : {
+        prix : 8,
+        quantité : 10
+    },
+    gants : {
+        prix : 4,
+        quantité : 10
+    },
+    marteau : {
+        prix : 16,
+        quantité : 10
+    },
+    epee : {
+        prix : 22,
+        quantité : 10
+    },
+    casque : {
+        prix : 18,
+        quantité : 10
+    },
+}
+
+const articlesName = Object.keys(articles);
+console.log(articlesName);
