1e42421325b5e96145d875bc8b2b9936367ddd89
ebran benjamin <ebran.b@gmail.com>
2022-07-01 14:07:09 +0200

    add input cart

--- js/javascript.js
+++ js/javascript.js
@@ -79,7 +79,10 @@ function addCart(event) {
         // console.log(this);
         li.innerHTML = "<img class='cart-img' src='"+this.src+"'>";
         li.innerHTML += "<div class='cart-art-info'><p>"+this.dataset.name+"</p>"+
-        "<p>"+articlesObj[this.dataset.name].prix + " PO</p></div>"; 
+        "<p>"+articlesObj[this.dataset.name].prix + " PO</p>"+
+        // "<div class='quantity-item'><button class='art-button-moins'>-</button>"+
+        "<input class='art-button'type='number'>";
+        // "<button class='art-button-plus'>+</button></div></div>";
         priceWithoutTaxe += articlesObj[this.dataset.name].prix;
         console.log(priceWithoutTaxe);
         document.getElementById("final-price").innerHTML = "Prix HT : " + priceWithoutTaxe + " PO";
